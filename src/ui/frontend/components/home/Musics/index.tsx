b371cf0a5963f2da2ab9f5b2a3d5b2eb090cb5d4
Lessa Apolinario <lessaapolinario@gmail.com>
2025-01-15 20:08:42 -0300

    Finish initial styles for musics

--- src/ui/frontend/components/home/Musics/index.tsx
+++ src/ui/frontend/components/home/Musics/index.tsx
@@ -1,5 +1,27 @@
+import classNames from 'classnames'
+import { FaPlus } from 'react-icons/fa6'
 import styles from './styles.module.scss'
 
 export function Musics() {
-  return <aside className={styles.container}>Lista de músicas</aside>
+  return (
+    <aside className={styles.container}>
+      <h2>Lista de músicas</h2>
+
+      <div className={styles.musics}></div>
+
+      <label
+        htmlFor='addMusic'
+        className={classNames(styles.addMusic, 'text-primary', 'bg-secondary')}
+      >
+        Adicionar música
+        <input
+          id='addMusic'
+          type='file'
+          placeholder='Adicionar música'
+          accept='audio/mp3,audio/*;capture=microphone'
+        />
+        <FaPlus />
+      </label>
+    </aside>
+  )
 }
