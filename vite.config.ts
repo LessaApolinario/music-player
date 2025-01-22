1689f2b3da614473c795ee6ea1bd9ffbd627c380
Lessa Apolinario <lessaapolinario@gmail.com>
2025-01-22 16:53:53 -0300

    Add vite allowed hosts config

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,8 @@ export default defineConfig({
     host: true,
     strictPort: true,
     port: 1987,
+    // eslint-disable-next-line
+    // @ts-ignore
+    allowedHosts: ['.ngrok-free.app'],
   },
 })
