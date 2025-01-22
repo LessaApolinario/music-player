386c485a3f5d36c6a0f0d9a275082124be4160ff
Lessa Apolinario <lessaapolinario@gmail.com>
2025-01-22 07:54:40 -0300

    Add removed musics component

--- src/ui/frontend/pages/home/index.tsx
+++ src/ui/frontend/pages/home/index.tsx
@@ -1,12 +1,17 @@
 import classNames from 'classnames'
+import { Musics } from '../../components/home/Musics'
 import { Player } from '../../components/home/Player'
+import { useWindowWidth } from '../../hooks/useWindowWidth'
 import styles from './styles.module.scss'
 
 export function HomePage() {
+  const { isMobileScreen } = useWindowWidth()
+
   return (
     <main
       className={classNames(styles.container, 'bg-secondary', 'text-primary')}
     >
+      {!isMobileScreen && <Musics />}
       <Player />
     </main>
   )
