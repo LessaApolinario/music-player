4530ce559f07413f03209236524c9300e10a9d61
Lessa Apolinario <lessaapolinario@gmail.com>
2025-01-22 09:06:52 -0300

    Add responsive layout and modal component

--- src/ui/frontend/layouts/DefaultLayout/index.tsx
+++ src/ui/frontend/layouts/DefaultLayout/index.tsx
@@ -1,20 +1,44 @@
 import classNames from 'classnames'
+import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Modal } from '../../components/base/Modal/Modal'
+import { Musics } from '../../components/home/Musics'
 import { useWindowWidth } from '../../hooks/useWindowWidth'
 import styles from './styles.module.scss'
 
 export function DefaultLayout() {
   const { isMobileScreen } = useWindowWidth()
+  const [isMusicsModalOpen, setIsMusicsModalOpen] = useState(false)
+
+  function openMusicsModal() {
+    setIsMusicsModalOpen(true)
+  }
+
+  function closeMusicsModal() {
+    setIsMusicsModalOpen(false)
+  }
 
   return (
     <div className={classNames(styles.container, 'bg-secondary')}>
       <header className={styles.mainHeader}>
         <h1 className='text-primary'>Music player from 2007</h1>
         {isMobileScreen && (
-          <button className={styles.showMusics}>Visualizar músicas</button>
+          <button className={styles.showMusics} onClick={openMusicsModal}>
+            Visualizar músicas
+          </button>
         )}
       </header>
       <Outlet />
+
+      {isMobileScreen && (
+        <Modal
+          isOpen={isMusicsModalOpen}
+          onClose={closeMusicsModal}
+          className={classNames(styles.musicsModal, 'bg-secondary')}
+        >
+          <Musics />
+        </Modal>
+      )}
     </div>
   )
 }
